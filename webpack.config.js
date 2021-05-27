44ff4300f90f58a0d4cc85f5fc76279db93bcf4b
vladi27 <vbulvakhter@gmail.com>
2021-05-27 15:46:18 -0700

    Remove Logger from production

--- webpack.config.js
+++ webpack.config.js
@@ -24,5 +24,12 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production"),
+      },
+    }),
+  ],
   devtool: "source-map",
 };
